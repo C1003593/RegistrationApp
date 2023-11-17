416b4e06377c9ec8254d1f23b6787461336e0dd5
c1003593 <c1003593@my.shu.ac.uk>
2023-11-17 19:17:48 +0000

    Created and implemented signals

--- users/models.py
+++ users/models.py
@@ -6,6 +6,6 @@ class studentAccount(models.Model):
     image = models.ImageField(default = 'default.png', upload_to= 'profile_pics')
 
     def __str__(self):
-        return f'{self.user.first_name}{self.user.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
 
 # Create your models here.
--- users/signals.py
+++ users/signals.py
@@ -4,8 +4,7 @@ from django.dispatch import receiver
 from .models import studentAccount
 
 @receiver(post_save, sender = User)
-def create_studentAccount(sender, instance, created, **kwargs):
+def create_studentaccount(sender, instance, created, **kwargs):
     if created:
         studentAccount.objects.create(user = instance)
-        def ready(self):
-            from .signals import create_profile
+        
