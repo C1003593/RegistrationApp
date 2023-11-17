977026cf9f125eedbca1e6848085c9ce68466e0b
c1003593 <c1003593@my.shu.ac.uk>
2023-11-17 17:08:35 +0000

    Implemented crispy forms

--- moduleRegistration/settings.py
+++ moduleRegistration/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ModuleRegistrationSystem.apps.ModuleregistrationsystemConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
