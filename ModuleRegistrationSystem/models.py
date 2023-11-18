3f5b434e155ee4cf6282243daf5beda32e0c9ffe
c1003593 <c1003593@my.shu.ac.uk>
2023-11-18 15:34:57 +0000

    made module model

--- ModuleRegistrationSystem/models.py
+++ ModuleRegistrationSystem/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Module(models.Model):
+    name = models.CharField(max_length= 20)
+    code = models.CharField(max_length= 4)
+    credit = models.IntegerField(choices= [(20, 20), (40, 40), (60, 60)])
+    catagory = models.CharField(max_length= 20, choices= [('In-person', 'In person'), ('Online', 'Online')])
+    description = models.TextField()
+    availability = models.BooleanField(default=True)
+    #coursesallowedtoattend = 
+
+    def __str__(self):
+        return f'{self.name}'
 
 # Create your models here.
--- ModuleRegistrationSystem/views.py
+++ ModuleRegistrationSystem/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Module
 
 def home(request):
 
@@ -14,7 +15,8 @@ def contact(request):
 
 def modules(request):
         
-        return render(request, 'ModuleRegistrationSystem/modules.html', {'title': 'Module list'})
+        module_list = {'modules': Module.objects.all(), 'title': 'List of modules'}
+        return render(request, 'ModuleRegistrationSystem/modules.html', module_list)
 
 
 # Create your views here.
