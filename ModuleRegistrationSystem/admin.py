112802ed07781519eea6a11ec182d4546c1ecb61
c1003593 <c1003593@my.shu.ac.uk>
2023-11-23 23:29:42 +0000

    Implemented link tables and used test data

--- ModuleRegistrationSystem/admin.py
+++ ModuleRegistrationSystem/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Module, Registration
+from .models import Module, Registration, Course, CourseModule, courseStudent
 
 admin.site.register(Module)
 admin.site.register(Registration)
-
+admin.site.register(Course)
+admin.site.register(CourseModule)
+admin.site.register(courseStudent)
 # Register your models here.
--- ModuleRegistrationSystem/models.py
+++ ModuleRegistrationSystem/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class Course(models.Model):
+    name = models.CharField(max_length = 20)
+    startDate = models.DateField(default = timezone.now)
+    duration = models.CharField(max_length = 10 ,choices= [('1 year', '1 year'), ('2 years', '2 years'), ('3 years', '3 years')])
+    description = models.TextField()
 
+    def __str__(self):
+        return f'{self.name}'
 
 class Module(models.Model):
     name = models.CharField(max_length= 20)
@@ -11,17 +18,28 @@ class Module(models.Model):
     category = models.CharField(max_length= 20, choices= [('In-person', 'In person'), ('Online', 'Online')])
     description = models.TextField()
     availability = models.BooleanField(default=True)
-    #coursesallowedtoattend = 
 
     def __str__(self):
         return f'{self.name}'
+    
+class CourseModule(models.Model):
+    course = models.ForeignKey(Course, null = True, related_name= 'courseModules', on_delete = models.CASCADE)
+    module = models.ForeignKey(Module, null = True, related_name= 'moduleCourses', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f'{self.module} is offered by {self.course}'
 
 class Registration(models.Model):
-    student = models.ForeignKey(User,null = True, related_name= 'student_registrations', on_delete = models.CASCADE)
-    module = models.ForeignKey(Module, null = True, related_name= 'module_registrations', on_delete = models.CASCADE)
+    student = models.ForeignKey(User,null = True, related_name= 'studentregistrations', on_delete = models.CASCADE)
+    module = models.ForeignKey(Module, null = True, related_name= 'moduleregistrations', on_delete = models.CASCADE)
     dateOfRegistration = models.DateTimeField(default = timezone.now)
 
     def __str__(self):
-        return f'{self.student} registered to {self.module}'
+        return f'{self.student} is registered to {self.module}'
 
+class courseStudent(models.Model):
+    student = models.ForeignKey(User,null = True, related_name= 'studentCourse', on_delete = models.CASCADE)
+    course = models.ForeignKey(Course, null = True, related_name= 'courseStudent', on_delete = models.CASCADE)
 
+    def __str__(self):
+        return f'{self.student} is registered on {self.course}'
