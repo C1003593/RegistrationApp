e4d8d7d7e3cba52e416a80f18bc3988afcbe1fb1
c1003593 <c1003593@my.shu.ac.uk>
2023-12-11 16:47:54 +0000

    Made it so users cant reregister to a course.

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from ModuleRegistrationSystem.models import Course
 from .models import studentAccount
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(label = 'Email address', help_text = 'Your SHU email address.')
+    course = forms.ModelChoiceField(queryset=Course.objects.all(), required = True)
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'course']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
@@ -19,5 +21,5 @@ class studentAccountUpdateForm(forms.ModelForm):
     DOB = forms.DateField(help_text = 'Enter in the format: yyyy-mm-dd')
     class Meta:
         model = studentAccount
-        fields = ['DOB', 'Address', 'City', 'Country', 'Course', 'image']
+        fields = ['DOB', 'Address', 'City', 'Country', 'image']
         
--- users/views.py
+++ users/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, studentAccountUpdateForm
 from django.contrib.auth.decorators import login_required
+from .models import studentAccount
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            user = form.save()  
+
+            student = studentAccount.objects.get(user=user)
+            student.Course = form.cleaned_data.get('course')
+            student.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! Now you can login!')
             return redirect('login')
@@ -22,7 +27,7 @@ def register(request):
         return render(request, 'users/register.html', {'form': form, 'title': 'Student Registration'})
 
 @login_required
-def studentAccount(request):
+def student_account(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance = request.user)
         p_form = studentAccountUpdateForm(request.POST, request.FILES, instance = request.user.studentaccount)
